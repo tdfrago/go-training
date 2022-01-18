c0952655f564aeec71ddc257a9d1f37af185cc14
Tyron Rex Frago <76909246+tdfrago@users.noreply.github.com>
2022-01-19 01:32:17 +0800

    Packing for Holiday

---
+++ fundamentals/12372_PackingForHoliday/main.go
@@ -0,0 +1,26 @@
+//Packing for the Holiday
+package main
+
+import "fmt"
+
+func main() {
+	var t, l, w, h int
+	_, err := fmt.Scan(&t)
+	if err == nil && (t >= 1 && t <= 100) {
+		for i := 1; i <= t; i++ {
+			_, err := fmt.Scan(&l, &w, &h)
+			if err == nil && (l >= 1 && l <= 50) && (w >= 1 && w <= 50) && (h >= 1 && h <= 50) {
+				if l <= 20 && w <= 20 && h <= 20 {
+					fmt.Printf("Case %v: good\n", i)
+				} else {
+					fmt.Printf("Case %v: bad\n", i)
+				}
+			}
+		}
+
+	}
+}
+
+//for cmd: go run main.go < 12372.in > 12372.out
+// for powershell: type .\12372.in | go run main.go > 12372.out
+//refer to https://www.udebug.com/ to verify inputs
