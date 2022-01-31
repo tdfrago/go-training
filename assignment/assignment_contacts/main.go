e24d06aa40032645ec2b297bab9339f6b44ea118
Tyron Rex Frago <76909246+tdfrago@users.noreply.github.com>
2022-01-31 12:18:10 +0800

    On-going assignment

---
+++ assignment/assignment_contacts/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Record struct {
+	ID       int
+	Last     string
+	First    string
+	Company  string
+	Address  string
+	Country  string
+	Position string
+}
+
+type Database struct {
+	nextID int
+	mu     sync.Mutex
+	recs   []Record
+}
+
+func main() {
+	db := &Database{recs: []Record{}}
+	http.ListenAndServe(":8080", db.handler())
+}
+
+func (db *Database) handler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var id int
+		if r.URL.Path == "/store" {
+			db.process(w, r)
+		} else if n, _ := fmt.Sscanf(r.URL.Path, "/store/%d", &id); n == 1 {
+			db.processID(id, w, r)
+		}
+	}
+}
+
+func (db *Database) process(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var rec Record
+		if err := json.NewDecoder(r.Body).Decode(&rec); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		db.mu.Lock()
+		rec.ID = db.nextID
+		db.nextID++
+		db.recs = append(db.recs, rec)
+		db.mu.Unlock()
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, "{\"success\": true}")
+	case "GET":
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(db.recs); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case "PUT":
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+	case "DELETE":
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (db *Database) processID(id int, w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+	case "DELETE":
+		exists := false
+		db.mu.Lock()
+		for j, item := range db.recs {
+			if id == item.ID {
+				db.recs = append(db.recs[:j], db.recs[j+1:]...)
+				exists = true
+				break
+			}
+		}
+		db.mu.Unlock()
+		w.Header().Set("Content-Type", "application/json")
+		if exists {
+			fmt.Fprintln(w, "{\"success\": true}")
+		} else {
+			fmt.Fprintln(w, "{\"success\": false}")
+		}
+	}
+}
